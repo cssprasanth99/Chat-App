d70ff90934a2b097fa22aa69f041ce63d579b87b
Sai Satya Prasanth Chamuturi <cssprasanth99@gmail.com>
2025-07-31 21:48:16 +0530

    server update

--- server/server.js
+++ server/server.js
@@ -53,5 +53,9 @@ app.use("/api/messages", messageRouter);
 // db config
 await connectDB();
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
+  server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
+
+export default server;
