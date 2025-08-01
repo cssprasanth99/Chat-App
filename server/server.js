5af513f952ed0ec8052060f12a49a7598566cd16
Sai Satya Prasanth Chamuturi <cssprasanth99@gmail.com>
2025-08-01 21:17:24 +0530

    vercel json fix

--- server/server.js
+++ server/server.js
@@ -57,5 +57,5 @@ if (process.env.NODE_ENV !== "production") {
   const PORT = process.env.PORT || 5000;
   server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 }
-
+ 
 export default server;
